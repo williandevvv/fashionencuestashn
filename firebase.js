c9ac2d72ebf3d49dba3c8f15bea925b6653d2330
MortySanchez <168171846+williandevvv@users.noreply.github.com>
2025-12-22 09:55:40 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,17 @@
-// Configuración e inicialización compartida de Firebase
-// Rellena el objeto firebaseConfig con los valores de tu proyecto.
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID",
+  apiKey: "AIzaSyCjWB8gBuXhkkiYqgJZD9WZnbP50qZsduo",
+  authDomain: "fashionencuestashn.firebaseapp.com",
+  projectId: "fashionencuestashn",
+  storageBucket: "fashionencuestashn.firebasestorage.app",
+  messagingSenderId: "945445364712",
+  appId: "1:945445364712:web:87a8309dba7cd9d6ef3a5d"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { app, auth, db };
